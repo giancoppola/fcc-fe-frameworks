b6ce67d6a3f5803910a7683024730c3ab1520e84
giancoppola <44214851+giancoppola@users.noreply.github.com>
2024-03-01 20:50:56 +0000

    get

--- server.ts
+++ server.ts
@@ -2,10 +2,15 @@ const express = require('express');
 import {Express, NextFunction, Request, Response} from 'express';
 const app: Express = express();
 
+// static files
 app.use('/dist', express.static(__dirname + '/dist'));
 app.use('/styles', express.static(__dirname + '/styles'));
 app.use(express.static(__dirname + '/src'));
+
 app.get('/', (req, res) => {
+    res.sendFile('/index.html')
+})
+app.get('/quotes', (req, res) => {
     res.sendFile('/index.html')
 })
 
