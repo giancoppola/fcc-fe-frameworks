a46bb242033321c42b4fc5c623aa9b7e2e6e23ca
Gian <giancoppola@hotmail.co.uk>
2024-03-04 15:19:38 +0000

    styles and structure

--- src/calculator.js
+++ src/calculator.js
@@ -3,31 +3,44 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var store = require('./drums-state.js').default;
-var drums_state_js_1 = require("./drums-state.js");
+var store = require('./calculator-state.js').default;
+var calculator_state_js_1 = require("./calculator-state.js");
 var react_redux_1 = require("react-redux");
 var react_1 = __importDefault(require("react"));
 var react_dom_1 = __importDefault(require("react-dom"));
 var marked = require('marked');
 var Controls = function (props) {
-    return (react_1.default.createElement("div", { className: "controls", id: "controls" }));
+    return (react_1.default.createElement("div", { className: "controls", id: "controls" },
+        react_1.default.createElement("button", { id: "clear", value: 'AC', className: "AC btn btn--red" }, "AC"),
+        react_1.default.createElement("button", { className: 'btn btn--light', id: "divide", value: "/" }, "/"),
+        react_1.default.createElement("button", { className: 'btn btn--light', id: "multiply", value: "x" }, "x"),
+        react_1.default.createElement("button", { className: 'btn', id: "seven", value: "7" }, "7"),
+        react_1.default.createElement("button", { className: 'btn', id: "eight", value: "8" }, "8"),
+        react_1.default.createElement("button", { className: 'btn', id: "nine", value: "9" }, "9"),
+        react_1.default.createElement("button", { className: 'btn btn--light', id: "subtract", value: "-" }, "-"),
+        react_1.default.createElement("button", { className: "btn", id: "four", value: "4" }, "4"),
+        react_1.default.createElement("button", { className: "btn", id: "five", value: "5" }, "5"),
+        react_1.default.createElement("button", { className: "btn", id: "six", value: "6" }, "6"),
+        react_1.default.createElement("button", { className: 'btn btn--light', id: "add", value: "+" }, "+"),
+        react_1.default.createElement("button", { className: "btn", id: "one", value: "1" }, "1"),
+        react_1.default.createElement("button", { className: "btn", id: "two", value: "2" }, "2"),
+        react_1.default.createElement("button", { className: "btn", id: "three", value: "3" }, "3"),
+        react_1.default.createElement("button", { className: "zero btn", id: "zero", value: "0" }, "0"),
+        react_1.default.createElement("button", { className: 'btn', id: "decimal", value: "." }, "."),
+        react_1.default.createElement("button", { className: "equals btn btn--blue", id: "equals", value: "=" }, "=")));
 };
 var Output = function (props) {
-    return (react_1.default.createElement("div", { className: "output-display", id: "output-display" }));
+    return (react_1.default.createElement("div", { className: "output-display", id: "display" }, props.output));
 };
 var Formula = function (props) {
     return (react_1.default.createElement("div", { className: "formula-display", id: "formula-display" }));
 };
-var Calculator = function (props) {
-    return (react_1.default.createElement("div", { id: "calculator", className: "calculator" },
-        react_1.default.createElement(Formula, null),
-        react_1.default.createElement(Output, null),
-        react_1.default.createElement(Controls, null)));
-};
 var App = function (props) {
     console.log(props);
-    return (react_1.default.createElement("div", { id: 'drum-machine', className: 'drum-machine' },
-        react_1.default.createElement(Calculator, null)));
+    return (react_1.default.createElement("div", { id: "calculator", className: "calculator" },
+        react_1.default.createElement(Formula, { formula: props.formula, output: props.output, update: props.update }),
+        react_1.default.createElement(Output, { formula: props.formula, output: props.output, update: props.update }),
+        react_1.default.createElement(Controls, { update: props.update })));
 };
 var AppWrapper = function (props) {
     return (react_1.default.createElement(react_redux_1.Provider, { store: store },
@@ -35,16 +48,15 @@ var AppWrapper = function (props) {
 };
 var mapStateToProps = function (state) {
     return {
-        playing: state.playing,
-        volume: state.volume,
-        samples: state.samples
+        formula: state.formula,
+        formulaHistory: state.formulaHistory,
+        output: state.output,
+        outputHistory: state.outputHistory
     };
 };
 var mapDispatchToProps = function (dispatch) {
     return {
-        updatePlaying: function (text) { return dispatch((0, drums_state_js_1.updatePlaying)(text)); },
-        updateVolume: function (volume) { return dispatch((0, drums_state_js_1.updateVolume)(volume)); },
-        updateSamples: function (samples) { return dispatch((0, drums_state_js_1.updateSamples)(samples)); }
+        update: function (formula, output) { return dispatch((0, calculator_state_js_1.update)(formula, output)); },
     };
 };
 var Container = (0, react_redux_1.connect)(mapStateToProps, mapDispatchToProps)(App);
--- src/calculator.tsx
+++ src/calculator.tsx
@@ -1,7 +1,7 @@
 
 import Redux, { Dispatch, Store, legacy_createStore } from 'redux';
-const store: Store = require('./drums-state.js').default;
-import {updatePlaying, updateSamples, updateVolume, iState, iSample} from './drums-state.js';
+const store: Store = require('./calculator-state.js').default;
+import {update, iState} from './calculator-state.js';
 import {ConnectedProps, MapStateToProps, Provider, connect} from 'react-redux';
 import React, {useState, useEffect, PropsWithoutRef} from 'react';
 import ReactDOM from 'react-dom';
@@ -10,14 +10,30 @@ const marked = require('marked')
 const Controls = (props: any) => {
     return (
         <div className="controls" id="controls">
-
+            <button id="clear" value='AC' className="AC btn btn--red">AC</button>
+            <button className='btn btn--light' id="divide" value="/">/</button>
+            <button className='btn btn--light' id="multiply" value="x">x</button>
+            <button className='btn' id="seven" value="7">7</button>
+            <button className='btn' id="eight" value="8">8</button>
+            <button className='btn' id="nine" value="9">9</button>
+            <button className='btn btn--light' id="subtract" value="-">-</button>
+            <button className="btn" id="four" value="4">4</button>
+            <button className="btn" id="five" value="5">5</button>
+            <button className="btn" id="six" value="6">6</button>
+            <button className='btn btn--light' id="add" value="+">+</button>
+            <button className="btn" id="one" value="1">1</button>
+            <button className="btn" id="two" value="2">2</button>
+            <button className="btn" id="three" value="3">3</button>
+            <button className="zero btn" id="zero" value="0">0</button>
+            <button className='btn' id="decimal" value=".">.</button>
+            <button className="equals btn btn--blue" id="equals" value="=">=</button>
         </div>
     )
 }
 const Output = (props: any) => {
     return (
-        <div className="output-display" id="output-display">
-
+        <div className="output-display" id="display">
+            {props.output}
         </div>
     )
 }
@@ -29,21 +45,13 @@ const Formula = (props: any) => {
     )
 }
 
-const Calculator = (props: any) => {
-    return (
-        <div id="calculator" className="calculator">
-            <Formula/>
-            <Output/>
-            <Controls/>
-        </div>
-    )
-}
-
 const App = (props: any) => {
     console.log(props);
     return (
-        <div id='drum-machine' className='drum-machine'>
-            <Calculator />
+        <div id="calculator" className="calculator">
+            <Formula formula={props.formula} output={props.output} update={props.update}/>
+            <Output formula={props.formula} output={props.output} update={props.update}/>
+            <Controls update={props.update}/>
         </div>
     )
 }
@@ -58,16 +66,15 @@ const AppWrapper = (props: any) => {
 
 const mapStateToProps: MapStateToProps<any, any, iState> = (state: iState) => {
     return {
-        playing: state.playing,
-        volume: state.volume,
-        samples: state.samples
+        formula: state.formula,
+        formulaHistory: state.formulaHistory,
+        output: state.output,
+        outputHistory: state.outputHistory
     }
 }
 const mapDispatchToProps = (dispatch: Dispatch) => {
     return {
-        updatePlaying: (text: string) => dispatch(updatePlaying(text)),
-        updateVolume: (volume: number) => dispatch(updateVolume(volume)),
-        updateSamples: (samples: Array<iSample>) => dispatch(updateSamples(samples))
+        update: (formula: string, output: string) => dispatch(update(formula, output)),
     }
 }
 
